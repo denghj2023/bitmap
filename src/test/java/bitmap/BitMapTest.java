e870341137d4925ce07967335067c701bea1f246
haijun <haijun@superads.cn>
2024-04-06 14:42:52 +0800

    ...

--- src/test/java/bitmap/BitMapTest.java
+++ src/test/java/bitmap/BitMapTest.java
@@ -1,13 +1,18 @@
 package bitmap;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Slf4j
 @SpringBootTest
@@ -15,6 +20,8 @@ class BitMapTest {
 
     @Resource
     RedisTemplate<String, LocalDateTime> redisTemplate;
+    @Resource
+    ElasticsearchRestTemplate elasticsearchRestTemplate;
 
     @Test
     void contextLoads() {
@@ -22,5 +29,9 @@ class BitMapTest {
         redisTemplate.opsForValue().set("test.now", now);
         LocalDateTime now2 = redisTemplate.opsForValue().get("test.now");
         Assertions.assertEquals(now, now2);
+
+        Object mapping = elasticsearchRestTemplate.indexOps(IndexCoordinates.of("user-event-2024.04.04"))
+                .getMapping();
+        log.debug("mapping: {}", JSON.toJSONString(mapping, JSONWriter.Feature.PrettyFormat));
     }
 }
